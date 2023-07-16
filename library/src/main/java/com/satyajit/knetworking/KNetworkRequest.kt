1f044ff60cfa5298e0626dfdda6bc3c2d3a4e4ef
Satyajit Das <dassatya1995@gmail.com>
2023-07-16 16:24:06 +0530

    Add RequestBuilders methods for Post-Request.

--- library/src/main/java/com/satyajit/knetworking/KNetworkRequest.kt
+++ library/src/main/java/com/satyajit/knetworking/KNetworkRequest.kt
@@ -174,6 +174,156 @@ class KNetworkRequest private constructor(
         }
     }
 
+    data class PostBuilder(private val url: String, val converter: ParserFactory) {
+        private val requestMethod: RequestMethod = RequestMethod.Post
+
+        private var priority: Priority = Priority.MEDIUM
+
+        private val requestID = getUniqueId(url)
+
+        private val callUrl = url
+
+        private var tag: String? = null
+
+        private var headersMap: HashMap<String, MutableList<String>> = hashMapOf()
+
+        private var queryParameterMap: HashMap<String, MutableList<String>> = hashMapOf()
+
+        private var pathParametersMap: HashMap<String, String> = hashMapOf()
+
+        private var cacheControl: CacheControl? = null
+
+        private var userAgent: String? = null
+
+        private var listener: Listener? = null
+
+        fun setPriority(priority: Priority) = apply { this.priority = priority }
+
+        fun setTag(tag: String) = apply { this.tag = tag }
+
+        fun setHeaders(key: String, value: String): PostBuilder {
+            var headerList: MutableList<String>? = headersMap[key]
+            if (headerList == null) {
+                headerList = mutableListOf()
+                headersMap[key] = headerList
+            }
+            if (!headerList.contains(value)) {
+                headerList.add(value)
+            }
+
+            return this.apply { this.headersMap[key] = headerList }
+        }
+
+
+        fun setHeaders(headerMap: HashMap<String, String>): PostBuilder {
+            return setHeaders(headerMap.toMap())
+        }
+
+        fun setHeaders(headerMap: Map<String, String>): PostBuilder {
+            headerMap.run {
+                for ((key, value) in this.entries) {
+                    setHeaders(key = key, value = value)
+                }
+            }
+
+            return this
+        }
+
+        fun setHeaders(T: Any): PostBuilder {
+            return this.apply {
+                setHeaders(
+                    converter.getStringMap(T)
+                )
+            }
+        }
+
+        fun setQueryParameter(key: String, value: String): PostBuilder {
+            var list: MutableList<String>? = queryParameterMap[key]
+
+            if (list == null) {
+                list = mutableListOf()
+                queryParameterMap[key] = list
+            }
+
+            if (!list.contains(value)) {
+                list.add(value)
+            }
+
+            return this@PostBuilder
+        }
+
+
+        fun setQueryParameter(queryParameterMap: HashMap<String, String>): PostBuilder {
+            return this@PostBuilder.setQueryParameter(queryParameterMap.toMap())
+        }
+
+        fun setQueryParameter(queryParameterMap: Map<String, String>): PostBuilder {
+            queryParameterMap.let {
+                for ((key, value) in it.entries) {
+                    setQueryParameter(key, value)
+                }
+            }
+            return this@PostBuilder
+        }
+
+        fun setQueryParameter(T: Any): PostBuilder {
+            T.run {
+                setQueryParameter(
+                    converter.getStringMap(this)
+                )
+            }
+            return this@PostBuilder
+        }
+
+        fun setPathParameter(key: String, value: String): PostBuilder {
+            this.pathParametersMap[key] = value
+            return this@PostBuilder
+        }
+
+        fun setPathParameter(pathParameterMap: Map<String, String>): PostBuilder {
+            this.pathParametersMap.putAll(pathParameterMap)
+            return this@PostBuilder
+        }
+
+        fun setPathParameter(pathParameterMap: HashMap<String, String>): PostBuilder {
+            return this@PostBuilder.setPathParameter(pathParameterMap.toMap())
+        }
+
+
+        fun setPathParameter(T: Any): PostBuilder {
+            T.apply {
+                setPathParameter(
+                    converter.getStringMap(this)
+                )
+            }
+
+            return this@PostBuilder
+        }
+
+        fun setCacheControl(cacheControl: CacheControl) =
+            apply { this@PostBuilder.cacheControl = cacheControl }
+
+        fun setUserAgent(userAgent: String) = apply { this@PostBuilder.userAgent = userAgent }
+
+
+        fun build(): KNetworkRequest {
+            return KNetworkRequest(
+                requestType = requestMethod,
+                priority = priority,
+                requestID = requestID,
+                url = callUrl,
+                tag = tag,
+                headers = headersMap,
+                queryParameterMap = queryParameterMap,
+                pathParametersMap = pathParametersMap,
+                cacheControl = cacheControl,
+                userAgent = userAgent,
+                listener = listener
+            )
+
+        }
+    }
+
     interface Listener {
         fun onSuccess(response: String)
 
